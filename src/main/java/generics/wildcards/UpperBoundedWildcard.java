f98e4789010df0c68fac78e2d87a52beaf503203
Adriana Suarez <13165418+kolumbien@users.noreply.github.com>
2022-02-17 09:34:02 +0100

    type inference

--- src/main/java/generics/wildcards/UpperBoundedWildcard.java
+++ src/main/java/generics/wildcards/UpperBoundedWildcard.java
@@ -1,6 +1,7 @@
 package generics.wildcards;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,9 +25,45 @@ public class UpperBoundedWildcard {
 
     }
 
+    public static double sumAll(List<? extends Number> list){
+        double sum=0;
+
+        for(Number n: list){
+            sum += n.doubleValue();
+        }
+
+        return sum;
+    }
+
+    public static void showAll(List<? extends Number> list){
+        for(Number n: list){
+            System.out.println(n);
+        }
+    }
+
     public static void main(String[] args) {
-        List<? extends Number> list1 = new ArrayList<>();
-       // list1.add(new Integer(12));errroorrr
+       /* List<? extends Number> list1 = new ArrayList<>();
+        list1.add(new Integer(12));errroorrr no funciona
+        cannot guarantee the list is pointing to
+        porque podria add(integer) luego add(Double)*/
+
+        //upper bounded wildcards, only read items, no add items
+        List<? extends Number> list1 = new ArrayList<Integer>();
+        List<? extends Number> list2 = new ArrayList<Double>();
+        List<? extends Number> list3 = new ArrayList<Float>();
+
+        showAll(list1);
+
+        List<Integer> nums = new ArrayList<>();
+        showAll(nums);
+
+        showAll(Arrays.asList(1,2,3));
+
+
+        //lower bounded wildcards
+        List<? super Number> listLower = new ArrayList<>();
+        listLower.add(new Integer(12));
+
     }
 
 }
